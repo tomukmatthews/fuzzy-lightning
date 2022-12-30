960f24cbf99f360feb78732c56cad59d3667c95d
Tom Matthews <tomukmatthews@gmail.com>
2022-12-30 23:23:39 +0000

    Fix typo

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 packages = ['fuzzy_lightning']
 
 package_data = {'': ['*'], 'fuzzy_lightning': ['cpp/*']}
 
 install_requires = [
-    'pandas>=1.5.2,<2.0.0',
     'pybind11>=2.10.1,<3.0.0',
     'scikit-learn>=1.1.3,<2.0.0',
     'scipy>=1.9.3,<2.0.0',
@@ -18,7 +17,7 @@ setup_kwargs = {
     'name': 'fuzzy-lightning',
     'version': '0.1.0',
     'description': 'Perform fast approximate string matching.',
-    'long_description': '# fuzzy-lightning\nFast approximate string matching.\n',
+    'long_description': '#fuzzy-lightning\nFast approximate string matching.\n',
     'author': 'Tom Matthews',
     'author_email': 'tomukmatthews@gmail.com',
     'maintainer': 'None',
@@ -27,9 +26,9 @@ setup_kwargs = {
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
-    'python_requires': '>=3.10.7',
+    'python_requires': '>=3.9.14,<4.0.0',
 }
-from build import *
+from build import build
 
 build(setup_kwargs)
 
