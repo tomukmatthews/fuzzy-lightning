3faca8d47f8dd0d77f58303d5716087acd197840
Tom Matthews <tomukmatthews@gmail.com>
2022-12-31 17:28:41 +0000

    Lax python package version requirements

--- setup.py
+++ setup.py
@@ -1,32 +1,31 @@
 # -*- coding: utf-8 -*-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 packages = ['fuzzy_lightning']
 
 package_data = {'': ['*'], 'fuzzy_lightning': ['cpp/*']}
 
 install_requires = [
-    'pybind11>=2.10.1,<3.0.0',
-    'scikit-learn>=1.1.3,<2.0.0',
-    'scipy>=1.9.3,<2.0.0',
-    'setuptools>=65.6.3,<66.0.0',
-    'sparse-dot-topn>=0.3.3,<0.4.0',
+    'pybind11>=2.6.0',
+    'scikit-learn>=1.0.2',
+    'scipy>=1.7.0',
+    'sparse-dot-topn>=0.3.1',
 ]
 
 setup_kwargs = {
     'name': 'fuzzy-lightning',
-    'version': '0.1.0',
+    'version': '0.1.2',
     'description': 'Perform fast approximate string matching.',
     'long_description': '#fuzzy-lightning\nFast approximate string matching.\n',
     'author': 'Tom Matthews',
     'author_email': 'tomukmatthews@gmail.com',
-    'maintainer': 'None',
-    'maintainer_email': 'None',
-    'url': 'None',
+    'maintainer': 'Tom Matthews',
+    'maintainer_email': 'tomukmatthews@gmail.com',
+    'url': 'https://github.com/tomukmatthews/fuzzy-lightning',
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
-    'python_requires': '>=3.9.1,<4.0.0',
+    'python_requires': '>=3.8.1,<4.0.0',
 }
 from build import build
 
