38f4112f9d15273e03efa04ee7937650c8eaddc4
Tom Matthews <tomukmatthews@gmail.com>
2023-01-20 17:55:07 +0000

    Fix setup.py - add pybind11 to install requires

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+CPP_VERSION = 11
+
+
+def build(setup_kwargs):
+    ext_modules = [
+        Pybind11Extension("lcs", ["fuzzy_lightning/cpp/lcs.cpp"], cxx_std=CPP_VERSION),
+        Pybind11Extension("edit_distance", ["fuzzy_lightning/cpp/edit_distance.cpp"], cxx_std=CPP_VERSION),
+    ]
+    setup_kwargs.update(
+        {
+            "ext_modules": ext_modules,
+            "cmd_class": {"build_ext": build_ext},
+            "zip_safe": False,
+        }
+    )
+
 
 packages = ['fuzzy_lightning']
 
@@ -31,7 +49,6 @@ setup_kwargs = {
     'install_requires': install_requires,
     'python_requires': '>=3.8.1,<4.0.0',
 }
-from build import build
 
 build(setup_kwargs)
 
